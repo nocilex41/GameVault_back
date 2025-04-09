a5185728c09e802dda00f2165bb146b5d402850b
Xenozn <vezinbastien@gmail.com>
2025-04-09 09:13:31 +0200

    findAll games

---
+++ src/Controller/GameController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Repository\GameRepository;
+
+final class GameController extends AbstractController
+{
+    #[Route('/api/game', name: 'app_game', methods: ['GET'])]
+    public function index(GameRepository $gameRepository): Response
+    {
+
+
+        $games = $gameRepository->findAll();
+
+
+        return $this->json($games, 200, [], ['groups' => 'game:read']);
+    }
+}
+
